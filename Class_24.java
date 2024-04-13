ff308b1bfa8f6aa29c811ca9e93f7dc1a249542f
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-13 07:56:15 -0700

    Ternary Operators

---
+++ Class_24.java
@@ -0,0 +1,14 @@
+// topic: Ternary Operator 
+
+class Class_24{
+
+	public static void main(String args[])
+	{
+		int a = 4; 
+		int b = 7;
+
+		int max = (a > b) ? a : b;
+
+		System.out.println(max);
+	}
+}
