8e050f5d9a6793a8a7a06d95810fd316a890a261
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-13 07:50:19 -0700

    Scanner Class in java

---
+++ Class_16.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+ class Class_16{
+
+    public static void main(String args[]) {
+     
+        int a;
+        Scanner obj = new Scanner(System.in);
+        
+        System.out.println("Enter a number : ");
+        a = obj.nextInt();
+        System.out.println(a);
+
+        int b ;
+        
+        
+    }
+}
