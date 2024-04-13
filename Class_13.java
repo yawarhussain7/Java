a1efecd5b1de7f61e87503fbf91a9a1a9320cc40
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-13 07:49:23 -0700

    Local variable in java

---
+++ Class_13.java
@@ -0,0 +1,10 @@
+
+public class Class_13 {
+
+    public static void main(String args[]) {
+       
+        int num1 = 4;
+        int num2 = 6;
+        System.out.print(num1 +" + "+ num2 +" = "+ (num1+num2));
+    }
+}
