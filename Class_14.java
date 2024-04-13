d222565a1b0791d5f5ef77c21fde000cdc97fea7
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-13 07:49:49 -0700

    Typecosting in java

---
+++ Class_14.java
@@ -0,0 +1,14 @@
+//Topic : TYpe costing 
+public class Class_14 {
+
+  
+    public static void main(String args[]) {
+       
+        double a = 5;
+        System.out.println("Implicit Type Costing : "+a);
+        
+        double b = 4.332;
+        System.out.println("Explicit Type Costing : "+(int)b);
+        
+    }
+}
