bbb2e2e57e6ad229d3197f1472b3c8fb86e99f99
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-13 07:56:43 -0700

    Bitwise Operators

---
+++ Class_25.java
@@ -0,0 +1,16 @@
+// topic: Bitwise Operators 
+
+class Class_25{
+
+	public static void main(String args[])
+	{
+		int a = 5 ,b = 7;
+
+		System.out.println("AND Operator : "+ (a & b));
+		System.out.println("Complement Operator : "+ (~a ));
+		System.out.println("OR Operator : "+ (a | b));
+		System.out.println("XOR Operator : "+ (a ^ b));
+
+
+	}
+}
