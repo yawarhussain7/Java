c6a460657cd73393bf10da488822efa4f2869616
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-15 00:19:08 -0700

    Dignols rectangle

---
+++ Pattern_02.java
@@ -0,0 +1,32 @@
+public class Pattern_02 {
+
+    public static void main(String args[]) {
+       
+        for(int i =1; i <= 7; i++)
+        {
+            for(int j = 1; j <= 7; j++)
+            {
+                if(i == 1 || j == 1 || i == 7 || j == 7|| j == i)
+                {
+                    System.out.print(" * ");
+                }
+                
+                else{
+                    System.out.print("   ");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+}
+
+//Output:
+
+ // *  *  *  *  *  *  *
+ // *  *              *
+ // *     *           *
+ // *        *        *
+ // *           *     *
+ // *              *  *
+ // *  *  *  *  *  *  *
