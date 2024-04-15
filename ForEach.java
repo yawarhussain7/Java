596ad6eb3295fe19e7416e701bccd6c602a61532
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-15 00:10:37 -0700

    ForEach loop in java

---
+++ ForEach.java
@@ -0,0 +1,14 @@
+// Topic: ForEach loop 
+
+class ForEach{
+
+	public static void main(String arg[])
+	{
+		int marks[] = {10,20,30,40,50};
+
+		for(int a:marks)
+		{
+			System.out.print(a+" ");
+		}
+	}
+}
