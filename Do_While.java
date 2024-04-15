673883bd7f64de163f712e5e4b6db087e1043220
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-15 00:06:22 -0700

    Do while loop

---
+++ Do_While.java
@@ -0,0 +1,25 @@
+
+public class Do_While {
+
+    public static void main(String args[]) {
+      
+      int  index = 1;
+        
+        do{
+            System.out.println(index);
+            index++;
+        }while(index <= 10);
+    
+    
+    System.out.println("Print table of 8 using Do-While");
+    
+    int number = 8;
+    int num = 1;
+    do{
+    System.out.println(number + " x "+ num +" = "+ number*num);
+    num++;
+    }while(num<= 10);
+
+}
+
+}
