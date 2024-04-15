619e9cd6b68c577ef360135151ab997f0c4da15a
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-15 00:14:57 -0700

    Diamond Pattern

---
+++ Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 7; i++) {
+           
+            for (int j = 1; j <= 7 - i; j++) {
+                System.out.print("   ");
+            }
+          
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        
+      
+        for (int i = 7 - 1; i >= 1; i--) {
+           
+            for (int j = 1; j <= 7 - i; j++) {
+                System.out.print("   ");
+            }
+          
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//Output:
+
+ //                   *
+ //                *  *  *
+ //             *  *  *  *  *
+ //          *  *  *  *  *  *  *
+ //       *  *  *  *  *  *  *  *  *
+ //    *  *  *  *  *  *  *  *  *  *  *
+ // *  *  *  *  *  *  *  *  *  *  *  *  *
+ //    *  *  *  *  *  *  *  *  *  *  *
+ //       *  *  *  *  *  *  *  *  *
+ //          *  *  *  *  *  *  *
+ //             *  *  *  *  *
+ //                *  *  *
+ //                   *
