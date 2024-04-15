c2bca9017e605c339740aa1d010cbd3d59c37b8e
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-15 01:06:39 -0700

    Full Oppsite left side triangle

---
+++ Pattern_15.java
@@ -0,0 +1,26 @@
+public class Pattern_15 {
+
+    public static void main(String args[]) {
+       
+       for(int i = 7; i >= 1; i--)
+       {
+           for(int j = 1; j <= i; j++)
+           {
+            
+                   System.out.print(" * ");
+             
+           }
+           System.out.println();
+       }
+    }
+}
+
+//Output:
+
+ // *  *  *  *  *  *  *
+ // *  *  *  *  *  *
+ // *  *  *  *  *
+ // *  *  *  *
+ // *  *  *
+ // *  *
+ // *
