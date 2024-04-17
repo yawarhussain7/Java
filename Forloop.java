51559ad81bee5c5216e7f4a0b31390371233c5e9
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-17 01:23:17 -0700

    Unknown files

---
+++ Forloop.java
@@ -0,0 +1,19 @@
+
+public class Forloop {
+
+    public static void main(String args[]) {
+      
+        System.out.print("Using forloop ");
+        for(int i = 0; i <= 10; i++)
+        {
+            System.out.println(i);
+        }
+        
+        System.out.print("Table of 3 using for loop");
+        int num = 3;
+        for(int i = 1; i<=10;i++)
+        {
+            System.out.println(num + "x" + i + " = " + num * i);
+        }
+    }
+}
---
+++ Pattern_08.java
@@ -0,0 +1,33 @@
+
+public class Pattern_08 {
+
+    public static void main(String args[]) {
+       
+        for(int i =1; i <= 7; i++)
+        {
+            for(int j = 1; j <= 7; j++)
+            {
+                if(i == 1 || j == 1 || j == 7 || i == 7|| i == ((7/2)+1))
+                {
+                    System.out.print(" * ");
+                }
+                
+                else{
+                    System.out.print("   ");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+}
+
+//OUtput:
+
+ // *  *  *  *  *  *  *
+ // *                 *
+ // *                 *
+ // *  *  *  *  *  *  *
+ // *                 *
+ // *                 *
+ // *  *  *  *  *  *  *
