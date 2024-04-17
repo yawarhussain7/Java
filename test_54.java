ad20e62a3b26928217b8282a1cc348731e0b7c23
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-17 01:16:30 -0700

    Chanage the number into alphabat

---
+++ test_54.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class test_54{
+	
+	public static void main(String args[])
+	{
+		Scanner input = new Scanner(System.in);
+		char letter;
+		System.out.println("Enter the any Alphabat : ");
+		letter = input.next().charAt(0);
+		System.out.println("Letter is : "+letter);
+	}
+}
