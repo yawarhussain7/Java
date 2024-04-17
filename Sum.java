5fc469101c27d59080d5c1dce1ef6337f2608558
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-17 01:22:20 -0700

    Add two numbers

---
+++ Sum.java
@@ -0,0 +1,11 @@
+
+public class Sum {
+
+    public static void main(String args[]) {
+        
+        int a = 4;
+        int b = 6;
+        int sum = a + b;
+        System.out.print(a +" + "+ b + " = "+sum);
+    }
+}
