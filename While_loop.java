d1d328f6312d4a44603cd5e200bdd958a90fffec
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-17 01:20:24 -0700

    While loop in java

---
+++ While_loop.java
@@ -0,0 +1,29 @@
+
+public class While_loop {
+
+    public static void main(String args[]) {
+        
+        int counter = 0;
+        
+        while(counter <= 10)
+        {
+            System.out.println(counter);
+            
+            counter++;
+        }
+        
+        System.out.print("Print table of 5 ");
+        
+        int index = 1;
+        int number = 5;
+        while(index <= 10)
+        {
+            System.out.println(number + " x "+ index +" = "+ number * index);
+            
+            index++;
+        }
+        
+      
+        
+    }
+}
